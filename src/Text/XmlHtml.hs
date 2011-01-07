7a145ff4a776dcc74774fc390b2ad0dccf9ff855
Chris Smith <cdsmith@gmail.com>
2011-01-06 19:47:58 -0700

    Fix more formatting for new documentation

--- src/Text/XmlHtml.hs
+++ src/Text/XmlHtml.hs
@@ -1,24 +1,23 @@
-{-|
-    Parsers and renderers for XML and HTML 5.  Although the formats are treated
-    differently, the data types used by each are the same, which makes it easy
-    to write code that works with the element structure of either XML or HTML 5
-    documents.
-    
-    Limitations:
-
-    * The XML parser does not parse internal DOCTYPE subsets.  If one is found,
-      the parsing will fail.
-
-    * The HTML 5 parser is not a compliant HTML parser.  Instead, it is a parser
-      for valid HTML 5 content.  It should only be used on content that you
-      have reason to believe is probably correct, since the compatibility
-      features of HTML 5 are missing.  This is the wrong library on which to
-      build a web spider.
-
-    * Both parsers accept fragments of documents, by which is meant that they
-      do not enforce the top-level structure of the document.  Files may
-      contain more than one root element, for example.
--}
+------------------------------------------------------------------------------
+-- | Parsers and renderers for XML and HTML 5.  Although the formats are
+--   treated differently, the data types used by each are the same, which
+--   makes it easy to write code that works with the element structure of
+--   either XML or HTML 5 documents.
+--
+--   Limitations:
+--
+--   * The XML parser does not parse internal DOCTYPE subsets.  If one is
+--     found, the parsing will fail.
+--
+--   * The HTML 5 parser is not a compliant HTML parser.  Instead, it is a
+--     parser for valid HTML 5 content.  It should only be used on content
+--     that you have reason to believe is probably correct, since the
+--     compatibility features of HTML 5 are missing.  This is the wrong
+--     library on which to build a web spider.
+--
+--   * Both parsers accept fragments of documents, by which is meant that
+--     they do not enforce the top-level structure of the document.  Files
+--     may contain more than one root element, for example.
 module Text.XmlHtml (
     -- * Types
     Document(..),
